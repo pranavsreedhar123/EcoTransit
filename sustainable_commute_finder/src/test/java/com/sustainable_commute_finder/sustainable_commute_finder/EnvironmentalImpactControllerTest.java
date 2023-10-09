b6d285ba20e9519835303e0857e101e217cc7c98
Minseung Jung <minseung@ipsec-10-2-192-41.vpn.gatech.edu>
2023-10-09 16:29:43 -0400

    Add the EnvironmentalImplactController Junit Test

---
+++ sustainable_commute_finder/src/test/java/com/sustainable_commute_finder/sustainable_commute_finder/EnvironmentalImpactControllerTest.java
@@ -0,0 +1,37 @@
+package com.sustainable_commute_finder.sustainable_commute_finder;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class EnvironmentalImpactControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setUp() {
+        // Perform any setup if needed
+    }
+
+    @Test
+    public void testCalculateEnvironmentalImpact() throws Exception {
+        double distance = 10.0;
+        String mode = "walking";
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/environmental-impact/{distance}/{mode}", distance, mode)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("$.commuteDistance").value(distance))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.commuteMode").value(mode))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.positiveImpact").value(1.0)); // Adjust the expected value as per your logic
+    }
+}
