10586b4e0371bc14a7c8fdb84e120a740dce923f
Bill Merickel <billmerickel@Bills-Laptop.local>
2023-10-10 17:45:50 -0400

    Move transportation mode drop down page to new UserVariables page

--- frontend/src/components/RouteValues.jsx
+++ frontend/src/components/RouteValues.jsx
@@ -16,7 +16,6 @@ const RouteValues = (props) => {
     durationW: "",
     distanceT: "",
     durationT: "",
-    transportationMode: "",
   });
 
   useEffect(() => {
@@ -33,17 +32,9 @@ const RouteValues = (props) => {
     getLocation();
   }, [location]);
 
-  const handleTransportationChange = (e) => {
-    const selectedMode = e.target.value;
-    setData((prevData) => ({
-      ...prevData,
-      transportationMode: selectedMode,
-    }));
-  };
-
   const navigate = useNavigate();
   const routeChange = () => {
-    let path = `/route`;
+    let path = `/user-variables`;
     navigate(path);
   };
 
@@ -99,32 +90,11 @@ const RouteValues = (props) => {
             <li>Distance: {data.distanceT}</li>
             <li>Duration: {data.durationT}</li>
           </Box>
-          <Box
-            bg="green.100"
-            minW={450}
-            borderColor={"gray"}
-            borderWidth="2px"
-            borderRadius="lg"
-            padding={2}
-          >
-            <h1>
-              <b>Mode of Transportation: </b>
-            </h1>
-            <FormControl>
-            <Select
-              onChange={handleTransportationChange}
-            >
-              <option value="Walking">Walking</option>
-              <option value="Biking">Biking</option>
-              <option value="Driving">Driving</option>
-              <option value="Public Transit">Public Transit</option>
-              <option value="Flying">Flying</option>
-            </Select>
-          </FormControl>
-          </Box>
+          
+          
           <Box paddingTop={50}>
             <Button onClick={routeChange} colorScheme="blue" width={450}>
-              Calculate Carbon Estimate
+              Continue to Input Variables
             </Button>
           </Box>
         </VStack>
